fdcf3149f1f656c548fcf60d55944818c9d1e414
adilkenzh17 <95138445+neobyvatel@users.noreply.github.com>
2023-11-10 14:58:30 +0600

    aboba

--- main.js
+++ main.js
@@ -15,10 +15,9 @@ const app = {
       .addEventListener("click", app.getLocation);
   },
   fetchWeather: (ev) => {
-    //use the values from latitude and longitude to fetch the weather
     let lat = document.getElementById("latitude").value;
     let lon = document.getElementById("longitude").value;
-    let key = "3287659a80ac8c72de4abd189fe90a9f";
+    let key = "e31236ca2959caf5178b8298a93073e8";
     let lang = "en";
     let units = "metric";
     let url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
@@ -42,6 +41,7 @@ const app = {
     navigator.geolocation.getCurrentPosition(app.ftw, app.wtf, opts);
   },
   ftw: (position) => {
+    console.log(position);
     //got position
     document.getElementById("latitude").value =
       position.coords.latitude.toFixed(2);
@@ -54,50 +54,36 @@ const app = {
   },
   showWeather: (resp) => {
     console.log(resp);
-    let row = document.querySelector(".weather.row");
-    //clear out the old weather and add the new
-    // row.innerHTML = '';
-    row.innerHTML = resp.daily
-      .map((day, idx) => {
-        if (idx <= 2) {
-          let dt = new Date(day.dt * 1000); //timestamp * 1000
-          let sr = new Date(day.sunrise * 1000).toTimeString();
-          let ss = new Date(day.sunset * 1000).toTimeString();
-          return `<div class="col">
-              <div class="card">
-              <h5 class="card-title p-2">${dt.toDateString()}</h5>
-                <img
-                  src="http://openweathermap.org/img/wn/${
-                    day.weather[0].icon
-                  }@4x.png"
-                  class="card-img-top"
-                  alt="${day.weather[0].description}"
-                />
-                <div class="card-body">
-                  <h3 class="card-title">${day.weather[0].main}</h3>
-                  <p class="card-text">High ${day.temp.max}&deg;C Low ${
-                    day.temp.min
-                  }&deg;C</p>
-                  <p class="card-text">High Feels like ${
-                    day.feels_like.day
-                  }&deg;C</p>
-                  <p class="card-text">Pressure ${day.pressure}mb</p>
-                  <p class="card-text">Humidity ${day.humidity}%</p>
-                  <p class="card-text">UV Index ${day.uvi}</p>
-                  <p class="card-text">Precipitation ${day.pop * 100}%</p>
-                  <p class="card-text">Dewpoint ${day.dew_point}</p>
-                  <p class="card-text">Wind ${day.wind_speed}m/s, ${
-                    day.wind_deg
-                  }&deg;</p>
-                  <p class="card-text">Sunrise ${sr}</p>
-                  <p class="card-text">Sunset ${ss}</p>
-                </div>
-              </div>
-            </div>
-          </div>`;
-        }
-      })
-      .join(" ");
+    // Update City
+    document.getElementById("city").textContent = `City: ${resp.timezone}`;
+
+    // Update Card 1: Main Temperature
+    document.getElementById(
+      "mainTemperature"
+    ).textContent = `Temperature: ${resp.current.temp} °C`;
+
+    // Update Card 2: Humidity
+    document.getElementById(
+      "humidity"
+    ).textContent = `Humidity: ${resp.current.humidity}%`;
+
+    // Update Card 3: Wind Information
+    document.getElementById(
+      "windSpeed"
+    ).textContent = `Speed: ${resp.current.wind_speed} m/s`;
+    document.getElementById(
+      "windDirection"
+    ).textContent = `Direction: ${resp.current.wind_deg}°`;
+
+    // Update Card 4: Pressure
+    document.getElementById(
+      "pressure"
+    ).textContent = `Pressure: ${resp.current.pressure} hPa`;
+
+    // Update Card 5: UV Index
+    document.getElementById(
+      "uvIndex"
+    ).textContent = `UV Index: ${resp.current.uvi}`;
   },
 };
 
