62fb52ea82516afd4d36a63b0f4d983487ba4f45
adilkenzh17 <95138445+neobyvatel@users.noreply.github.com>
2023-11-13 00:19:13 +0600

    unnecessary update

--- main.js
+++ main.js
@@ -127,3 +127,39 @@ document.addEventListener("DOMContentLoaded", function () {
   const formattedDate = currentDate.toLocaleDateString("en-US", options);
   currentDateElement.textContent = `Current Date: ${formattedDate}`;
 });
+
+// clock.js
+
+setInterval(function () {
+  const clock = document.querySelector(".display");
+
+  let time = new Date();
+
+  let sec = time.getSeconds();
+  let min = time.getMinutes();
+  let hr = time.getHours();
+  let day = "AM";
+
+  if (hr > 12) {
+    day = "PM";
+    hr = hr - 12;
+  }
+
+  if (hr == 0) {
+    hr = 12;
+  }
+
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+
+  if (min < 10) {
+    min = "0" + min;
+  }
+
+  if (hr < 10) {
+    hr = "0" + hr;
+  }
+
+  clock.textContent = hr + ":" + min + ":" + sec + " " + day;
+}, 1000); // Update every 1000 milliseconds (1 second)
