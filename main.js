801638473d2a43be7f5a7aee32773ea720831f1e
adilkenzh17 <95138445+neobyvatel@users.noreply.github.com>
2023-11-24 01:28:20 +0600

    forecast data fetch

--- main.js
+++ main.js
@@ -15,55 +15,83 @@ function fetchWeather() {
   let key = "e31236ca2959caf5178b8298a93073e8";
   let lang = "en";
   let units = "metric";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=${lang}&units=${units}`;
-  //fetch the weather
-  fetch(url)
+
+  // Fetch current weather data
+  let currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=${lang}&units=${units}`;
+  fetch(currentWeatherUrl)
     .then((resp) => {
       if (!resp.ok) throw new Error(resp.statusText);
       return resp.json();
     })
-    .then((data) => {
-      showWeather(data);
+    .then((currentWeatherData) => {
+      // Fetch 5-day forecast data
+      let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&lang=${lang}&units=${units}`;
+      return fetch(forecastUrl)
+        .then((resp) => {
+          if (!resp.ok) throw new Error(resp.statusText);
+          return resp.json();
+        })
+        .then((forecastData) => {
+          showWeather(currentWeatherData, forecastData);
+        });
     })
     .catch((error) => {
       console.log(error);
       alert("City not found. Please check the city name and try again.");
     });
+
   cityNameInput.value = "";
 }
-function showWeather(resp) {
-  console.log(resp);
-  console.log(resp.daily);
-  document.getElementById("updateTime").textContent = getCurrentTime(); // Assuming you have a function getCurrentTime() to get the current time
+
+function showWeather(currentWeatherData, forecastData) {
+  console.log("Current Weather:", currentWeatherData);
+  console.log("5-Day Forecast:", forecastData.list); // This will log the list of forecasts
+
+  // Display current weather data
+  document.getElementById("updateTime").textContent = getCurrentTime();
   document.getElementById(
     "city"
-  ).textContent = `${resp.name}, ${resp.sys.country}`;
-  document.getElementById("temp").innerHTML = `${resp.main.temp}&deg;C`;
+  ).textContent = `${currentWeatherData.name}, ${currentWeatherData.sys.country}`;
+  document.getElementById(
+    "temp"
+  ).innerHTML = `${currentWeatherData.main.temp}&deg;C`;
   document.getElementById(
     "feelsLike"
-  ).innerHTML = `${resp.main.feels_like}&deg;C`;
+  ).innerHTML = `${currentWeatherData.main.feels_like}&deg;C`;
   document.getElementById(
     "windSpeed"
-  ).textContent = `Wind speed: ${resp.wind.speed} m/s`;
+  ).textContent = `Wind speed: ${currentWeatherData.wind.speed} m/s`;
   document.getElementById(
     "windDirection"
-  ).textContent = `Direction: ${resp.wind.deg}°`;
+  ).textContent = `Direction: ${currentWeatherData.wind.deg}°`;
   document.getElementById("weatherDescription").textContent =
-    resp.weather[0].description;
-  document.getElementById("humidity").textContent = `${resp.main.humidity}`;
-  document.getElementById("pressure").textContent = `${resp.main.pressure} hPa`;
-  const sunriseTime = formatTimeFromTimestamp(resp.sys.sunrise);
-  const sunsetTime = formatTimeFromTimestamp(resp.sys.sunset);
-
+    currentWeatherData.weather[0].description;
+  document.getElementById(
+    "humidity"
+  ).textContent = `${currentWeatherData.main.humidity}`;
+  document.getElementById(
+    "pressure"
+  ).textContent = `${currentWeatherData.main.pressure} hPa`;
+  const sunriseTime = formatTimeFromTimestamp(currentWeatherData.sys.sunrise);
+  const sunsetTime = formatTimeFromTimestamp(currentWeatherData.sys.sunset);
   document.getElementById("sunrise").textContent = `${sunriseTime}`;
   document.getElementById("sunset").textContent = `${sunsetTime}`;
+
+  // Iterate through the forecast data and display it
+  forecastData.list.forEach((forecast) => {
+    console.log(
+      `Date/Time: ${forecast.dt_txt}, Temperature: ${forecast.main.temp}&deg;C, Weather: ${forecast.weather[0].description}`
+    );
+  });
 }
+
 function getCurrentTime() {
   const currentTime = new Date();
   const hours = currentTime.getHours();
   const minutes = currentTime.getMinutes();
   return `${hours}:${minutes < 10 ? "0" : ""}${minutes}`;
 }
+
 function formatTimeFromTimestamp(timestamp) {
   const date = new Date(timestamp * 1000); // Convert timestamp to milliseconds
   const hours = date.getHours();
